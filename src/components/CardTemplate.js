ae532fbca9f12a5df3be4d23b58da393638904c0
TKadyear <tamarakadyear@gmail.com>
2022-04-10 13:11:42 +0200

    Added the display of the Card in mobile view with fake data

--- src/components/CardTemplate.js
+++ src/components/CardTemplate.js
@@ -3,13 +3,88 @@ import { device } from "../device";
 
 const ContainerCard = styled.div`
   background: var(--bg-cards);
-  width: 100%;
-  height: 100px;
+  padding: 2rem 1rem;
+  position: relative;
   border-radius: 0.25rem;
   box-shadow: 0 6px 16px var(--shadow-primary);
 `;
+const TitleCompany = styled.p`
+  display: inline;
+  margin-right:1rem ;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  color: var(--primary);
+  font-weight: bold;
+`
+const ImageCompany = styled.img`
+  position: absolute;
+  width: 3rem;
+  top: -1.5rem;
+  left: 1.5rem;
+`
+const ContainerTags = styled.div`
+  display: inline;
+  height: max-content;
 
-export const CardTemplate = () => {
+`
+// IMPROVE the way to pass props in these tags
+const Tag = styled.span`
+  display: inline;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  background-color: ${props => props.new ? 'var(--primary)' : 'var(--darker-bg)'} ;
+  padding: 4px 8px;
+  margin: 0 0.25rem;
+  border-radius: 12px;
+  color: white;
+  font-weight: bold;
+  font-size: 12px;
+`
+const JobPosition = styled.p`
+  font-size: 1rem;
+  font-weight: bold;
+`
+const DetailsJob = styled.span`
+  color: var(--dark-bg);
+`
+const Divider = styled.hr`
+  border: 0;
+  height: 0.75px;
+  background: var(--dark-bg);
+  margin: 1rem auto;
+  @media ${device.tablet}{
+    display:none;
+  }
+`
 
-  return <ContainerCard ></ContainerCard>;
+const DevTag = styled.p`
+  display: inline;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  background-color: var(--bg-primary);
+  padding: 4px 8px;
+  margin: 0 0.5rem;
+  border-radius: 4px;
+  color: var(--primary);
+  font-weight: bold;
+  font-size: 14px;
+`
+export const CardTemplate = (props) => {
+  const job = props.data;
+  const LanguagesRequired = () => job.languages.map(language =>
+    <DevTag key={job.company + language}>{language}</DevTag>
+  )
+  const detail = `${job.postedAt} · ${job.contract} · ${job.location}`
+  return (
+    <ContainerCard>
+      <ImageCompany src={job.logo} />
+      <TitleCompany>{job.company}</TitleCompany>
+      <ContainerTags>
+        {job.new && <Tag new={job.new}>NEW!</Tag>}
+        {job.featured && <Tag new={false}>FEATURED</Tag>}
+      </ContainerTags>
+      <JobPosition>{job.position}</JobPosition>
+      <DetailsJob>{detail}</DetailsJob>
+      <Divider />
+      {LanguagesRequired()}
+
+    </ContainerCard >
+  );
 }
--- src/components/CardsContainer.js
+++ src/components/CardsContainer.js
@@ -3,18 +3,33 @@ import { device } from "../device";
 import { CardTemplate } from "./CardTemplate";
 const Container = styled.div`
   width: 90%;
-  margin: 1rem auto;
+  margin: 3.5rem auto;
   display: flex;
   flex-direction: column;
-  gap: 2rem;
+  gap: 2.625rem;
 `;
 
 export const CardsContainer = () => {
+  const data = {
+    "id": 1,
+    "company": "Photosnap",
+    "logo": "./images/photosnap.svg",
+    "new": true,
+    "featured": true,
+    "position": "Senior Frontend Developer",
+    "role": "Frontend",
+    "level": "Senior",
+    "postedAt": "1d ago",
+    "contract": "Full Time",
+    "location": "USA Only",
+    "languages": ["HTML", "CSS", "JavaScript"],
+    "tools": []
+  }
 
   return (
     <Container>
-      <CardTemplate></CardTemplate>
-      <CardTemplate></CardTemplate>
+      <CardTemplate data={data} />
+      <CardTemplate data={data} />
     </Container>
   )
 }
