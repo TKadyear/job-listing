c56794db5730358439ed53e160d6dc4c76a332a7
TKadyear <tamarakadyear@gmail.com>
2022-04-10 11:26:23 +0200

    Added device object for media queries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-
+import { device } from './device'
 
 const Title = styled.h1`
     font-size: 1.5em;
@@ -8,12 +8,21 @@ const Title = styled.h1`
     color: white;
   `;
 const ContainerMain = styled.div`
-  margin: 0 auto;
-  background: var(--primary);
+  height: 100vh;
+  background: var(--bg-primary);
+`;
+const BackgroundMain = styled.div`
+    height: 156px;
+    background: url(/public/images/bg-header-mobile.svg) var(--primary);
+  @media ${device.tablet} {
+    height: 100vh;
+    background: url(/public/images/bg-header-desktop.svg) var(--primary);
+  }
 `;
 function App() {
   return (
     <ContainerMain>
+      <BackgroundMain />
       <Title>This is Style Components</Title>
     </ContainerMain>
   );
