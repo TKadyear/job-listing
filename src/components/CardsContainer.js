3fb464e2f8bad102e593e838744a11b2b1c6320c
TKadyear <tamarakadyear@gmail.com>
2022-04-10 13:19:35 +0200

    Added fetch to the data

--- src/components/CardsContainer.js
+++ src/components/CardsContainer.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { device } from "../device";
 import { CardTemplate } from "./CardTemplate";
@@ -10,26 +11,18 @@ const Container = styled.div`
 `;
 
 export const CardsContainer = () => {
-  const data = {
-    "id": 1,
-    "company": "Photosnap",
-    "logo": "./images/photosnap.svg",
-    "new": true,
-    "featured": true,
-    "position": "Senior Frontend Developer",
-    "role": "Frontend",
-    "level": "Senior",
-    "postedAt": "1d ago",
-    "contract": "Full Time",
-    "location": "USA Only",
-    "languages": ["HTML", "CSS", "JavaScript"],
-    "tools": []
-  }
+  const [listData, setListData] = useState([])
+  useEffect(() => {
+    fetch("./data.json")
+      .then(response => response.json())
+      .then(data => setListData(data));
+  }, [])
 
   return (
     <Container>
-      <CardTemplate data={data} />
-      <CardTemplate data={data} />
+      {listData.map(job =>
+        <CardTemplate key={job.id} data={job} />
+      )}
     </Container>
   )
 }
