329f9d20df4aa36c560b9a0bde48d8a2c478e845
TKadyear <tamarakadyear@gmail.com>
2022-04-10 13:22:51 +0200

    Remove Title from App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import styled from 'styled-components';
 import { device } from './device'
 import { CardsContainer } from './components/CardsContainer'
-const Title = styled.h1`
-    margin: 0;
-    font-size: 1.5em;
-    font-family: 'Spartan';
-    text-align: center;
-    color: white;
-  `;
 const ContainerMain = styled.div`
   height: 100vh;
   background: var(--bg-primary);
@@ -23,7 +16,6 @@ function App() {
   return (
     <ContainerMain>
       <BackgroundMain>
-        <Title>This is Style Components</Title>
       </BackgroundMain>
       <CardsContainer></CardsContainer>
     </ContainerMain>
