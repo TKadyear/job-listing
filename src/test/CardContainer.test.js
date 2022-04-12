aada44da449412e3e9d34d1e6e37ff9605586a4d
TKadyear <tamarakadyear@gmail.com>
2022-04-12 12:04:30 +0200

    Added the mock to the test

---
+++ src/test/CardContainer.test.js
@@ -0,0 +1,6 @@
+const CardsContainer = require("../components/CardsContainer");
+const requestJob = require("../service/requestJobs");
+jest.mock("../service/requestJobs");
+describe('First test', () => {
+
+})
